e608593747afd280ca4fd5c266e93d82693a8ec4
agreeablyblue <65247826+agreeablyblue@users.noreply.github.com>
2020-05-13 16:25:04 -0400

    Update render_object.js

--- render_object.js
+++ render_object.js
@@ -19,20 +19,26 @@ window.addEventListener( 'resize', function()
   camera.updateProjectionMatrix( );
 })
 
+//Creates a grid behind the object being rendered
+var size = 10;
+var divisions = 10;
 
+var gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper )
 
 //Generates the shape rendered, and gives it a mesh
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-
+cube.position.y = .5;
 //Pulls the camera back from the rendered shape so the shape is in view
-camera.position.z = 5;
-
+camera.position.z = 3;
+camera.position.y = 1;
 //Animation function which gives the cube some rotation
 var animate = function ( ) {
 requestAnimationFrame( animate );
+
 /*
 cube.rotation.x += 0.01;
 cube.rotation.y += 0.01;
