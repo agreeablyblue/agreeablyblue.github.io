2dd6e30a369e28f45c864a67124819225b97c684
agreeablyblue <65247826+agreeablyblue@users.noreply.github.com>
2020-05-13 13:24:59 -0400

    Update render_object.js

--- render_object.js
+++ render_object.js
@@ -1,23 +1,40 @@
-var scene = new THREE.Scene();
+//Scene every object is placed into
+var scene = new THREE.Scene( );
+
+//Camera which defines how the object is viewed
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth * .5, window.innerHeight * .5 );
+//Establishes the renderer defined by three js
+var renderer = new THREE.WebGLRenderer( );
+renderer.setSize( window.innerWidth , window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//Scalable window resizing
+window.addEventListener( 'resize', function()
+{
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  renderer.setSize( width, height );
+  camera.aspect = width/height;
+  camera.updateProjectionMatrix( );
+})
+
+//Generates the shape rendered, and gives it a mesh
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+//Pulls the camera back from the rendered shape so the shape is in view
 camera.position.z = 5;
 
-var animate = function () {
+//Animation function which gives the cube some rotation
+var animate = function ( ) {
 requestAnimationFrame( animate );
-
+/*
 cube.rotation.x += 0.01;
 cube.rotation.y += 0.01;
-
+*/
 renderer.render( scene, camera );
 };
 
