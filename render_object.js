fb19680f6bdf84a2a73a91e18eedac29bd6267ed
agreeablyblue <65247826+agreeablyblue@users.noreply.github.com>
2020-05-21 17:16:00 -0400

    Skybox/PlaneMesh
    Added cloudy skybox, it's not the best looking so it'll be changed eventually but that's just a matter of swapping names in the script for the images to render. Added a plane mesh and linked the transform controls to it. The planes MTL isn't loading properly, that's next on the debug list.

--- render_object.js
+++ render_object.js
@@ -2,13 +2,13 @@
 var scene = new THREE.Scene( );
 
 //Camera which defines how the object is viewed
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 10000 );
 
 //Establishes the renderer defined by three js, sets the width to 80% of the screen, and the height to 95% of the screen. It also changes the background color of the renderer window to light grey
 var renderer = new THREE.WebGLRenderer( );
 renderer.setSize( window.innerWidth * 0.79 , window.innerHeight * 0.95 );
 document.body.appendChild( renderer.domElement );
-renderer.setClearColor("#e5e5e5");
+renderer.setClearColor("#efefef");
 
 //Scalable window resizing
 window.addEventListener( 'resize', function()
@@ -33,28 +33,80 @@ transform.addEventListener('dragging-changed', function(event){
   orbit.enabled = ! event.value;
 });
 
+//Mtl loader testing
+var mesh = null;
+
+var mtlLoader = new THREE.MTLLoader();
+mtlLoader.setPath( "threejs_example_project/assets/" );
+mtlLoader.load( 'privateJet.mtl', function( materials ) {
+
+  materials.preload();
+
+  var objLoader = new THREE.OBJLoader();
+  objLoader.setMaterials( materials );
+  objLoader.setPath( "threejs_example_project/assets/" );
+  objLoader.load( 'privateJet.obj', function ( object ) {
+
+    mesh = object;
+    mesh.position.y = -30;
+    scene.add( mesh );
+    transform.attach(mesh);
+    scene.add(transform);
+    transform.setMode("rotate");
+  } );
+
+} );
+
+//Skybox
+var materialArray = [];
+var tex_ft = new THREE.TextureLoader().load('threejs_example_project/assets/skybox/clouds1_front.png');
+var tex_bk = new THREE.TextureLoader().load('threejs_example_project/assets/skybox/clouds1_back.png');
+var tex_up = new THREE.TextureLoader().load('threejs_example_project/assets/skybox/clouds1_up.png');
+var tex_dn = new THREE.TextureLoader().load('threejs_example_project/assets/skybox/clouds1_down.png');
+var tex_rt = new THREE.TextureLoader().load('threejs_example_project/assets/skybox/clouds1_right.png');
+var tex_lf = new THREE.TextureLoader().load('threejs_example_project/assets/skybox/clouds1_left.png');
+
+
+
+materialArray.push(new THREE.MeshBasicMaterial({map: tex_ft}));
+materialArray.push(new THREE.MeshBasicMaterial({map: tex_bk}));
+materialArray.push(new THREE.MeshBasicMaterial({map: tex_up}));
+materialArray.push(new THREE.MeshBasicMaterial({map: tex_dn}));
+materialArray.push(new THREE.MeshBasicMaterial({map: tex_rt}));
+materialArray.push(new THREE.MeshBasicMaterial({map: tex_lf}));
+
+for (var i = 0; i<6; i++)
+  materialArray[i].side = THREE.BackSide;
+
+
+var skyboxGeo = new THREE.BoxGeometry(10000,10000,10000);
+var skybox = new THREE.Mesh(skyboxGeo, materialArray);
+scene.add(skybox);
+
 //Grid Helper creator
-var size = 10;
-var divisions = 10;
+var size = 850;
+var divisions = 100;
 var gridHelper = new THREE.GridHelper( size, divisions );
 scene.add( gridHelper )
 
+/*
 //Generates the shape rendered, and gives it a mesh
 var cube = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
   new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('textures/airplane.png')})
 );
-
+*/
+/*
 cube.material.side = THREE.DoubleSide;
 scene.add(cube);
+*/
 
 //Attaches transform controls to the rendererd shape, adds control handles to the scene, and sets the control mode to rotation
-transform.attach(cube);
-scene.add(transform);
-transform.setMode("rotate");
+
 
 //Pulls the camera back from the rendered shape so the shape is in view
-camera.position.z = 3;
+camera.position.z = 350;
+
 
 //Ambient light generator
 var ambientLight = new THREE.AmbientLight( 0xFFFFFF, 5.0 );
